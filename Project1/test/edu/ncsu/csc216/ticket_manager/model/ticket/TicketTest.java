b8198f8b2e607c4916711ba1317c3c00e6a8629d
Will Greene <wgreene@ncsu.edu>
2022-06-30 13:55:16 -0400

    [Implementation] Changed Spelling of "Cancelled" to "Canceled" v2

--- Project1/test/edu/ncsu/csc216/ticket_manager/model/ticket/TicketTest.java
+++ Project1/test/edu/ncsu/csc216/ticket_manager/model/ticket/TicketTest.java
@@ -356,13 +356,13 @@ public class TicketTest {
 		Ticket t3 = new Ticket(ID, "Feedback", TICKET_TYPE_STRING, SUBJECT, CALLER, CATEGORY_STRING, PRIORITY_STRING, OWNER, Command.F_CALLER, notes);
 		Ticket t4 = new Ticket(ID, "Resolved", TICKET_TYPE_STRING, SUBJECT, CALLER, CATEGORY_STRING, PRIORITY_STRING, OWNER, Command.RC_CALLER_CLOSED, notes);
 		Ticket t5 = new Ticket(ID, "Closed", TICKET_TYPE_STRING, SUBJECT, CALLER, CATEGORY_STRING, PRIORITY_STRING, OWNER, CODE, notes);
-		Ticket t6 = new Ticket(ID, "Cancelled", TICKET_TYPE_STRING, SUBJECT, CALLER, CATEGORY_STRING, PRIORITY_STRING, OWNER, Command.CC_DUPLICATE, notes);
+		Ticket t6 = new Ticket(ID, "Canceled", TICKET_TYPE_STRING, SUBJECT, CALLER, CATEGORY_STRING, PRIORITY_STRING, OWNER, Command.CC_DUPLICATE, notes);
 		
 		assertEquals("*" + ID + "#" + "New" + "#" + TICKET_TYPE_STRING + "#" + SUBJECT + "#" + CALLER + "#" + CATEGORY_STRING + "#" + PRIORITY_STRING + "#" + OWNER + "#" + CODE + "\n" + t1.getNotes(), t1.toString());
 		assertEquals("*" + ID + "#" + "Working" + "#" + TICKET_TYPE_STRING + "#" + SUBJECT + "#" + CALLER + "#" + CATEGORY_STRING + "#" + PRIORITY_STRING + "#" + OWNER + "#" + CODE + "\n" + t2.getNotes(), t2.toString());
 		assertEquals("*" + ID + "#" + "Feedback" + "#" + TICKET_TYPE_STRING + "#" + SUBJECT + "#" + CALLER + "#" + CATEGORY_STRING + "#" + PRIORITY_STRING + "#" + OWNER + "#" + Command.F_CALLER + "\n" + t3.getNotes(), t3.toString());
 		assertEquals("*" + ID + "#" + "Resolved" + "#" + TICKET_TYPE_STRING + "#" + SUBJECT + "#" + CALLER + "#" + CATEGORY_STRING + "#" + PRIORITY_STRING + "#" + OWNER + "#" + Command.RC_CALLER_CLOSED + "\n" + t4.getNotes(), t4.toString());
 		assertEquals("*" + ID + "#" + "Closed" + "#" + TICKET_TYPE_STRING + "#" + SUBJECT + "#" + CALLER + "#" + CATEGORY_STRING + "#" + PRIORITY_STRING + "#" + OWNER + "#" + CODE + "\n" + t5.getNotes(), t5.toString());
-		assertEquals("*" + ID + "#" + "Cancelled" + "#" + TICKET_TYPE_STRING + "#" + SUBJECT + "#" + CALLER + "#" + CATEGORY_STRING + "#" + PRIORITY_STRING + "#" + OWNER + "#" + Command.CC_DUPLICATE + "\n" + t6.getNotes(), t6.toString());
+		assertEquals("*" + ID + "#" + "Canceled" + "#" + TICKET_TYPE_STRING + "#" + SUBJECT + "#" + CALLER + "#" + CATEGORY_STRING + "#" + PRIORITY_STRING + "#" + OWNER + "#" + Command.CC_DUPLICATE + "\n" + t6.getNotes(), t6.toString());
 	}
 }
